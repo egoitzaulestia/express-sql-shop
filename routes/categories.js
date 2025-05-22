eb3d77c33b3506f95b773b10f32829e3bc363077
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-23 01:51:43 +0200

    add (post) category endpoint created

--- routes/categories.js
+++ routes/categories.js
@@ -16,4 +16,17 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// ADD CATEGORY (by POST method)
+router.post("/", (req, res) => {
+  const newCategory = req.body.name;
+  const sql = `INSERT INTO category (name) 
+    VALUES ('${newCategory}')`;
+
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send({ message: "New category added successfully.", result });
+  });
+});
+
 module.exports = router;

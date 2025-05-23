975d2db13c4844bbeac0d85ea284021b4b056cce
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-23 13:27:41 +0200

    user controller object created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,34 @@
 const db = require("../config/database");
 
 const UserController = {
+  create(req, res) {
+    const { username, email } = req.body;
+    const sql = `
+    INSERT INTO user (username, email)
+    VALUES (?, ?);`;
+
+    db.query(sql, [username, email], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send({ message: "New user registered successfully", result });
+    });
+  },
+
+  update(req, res) {
+    const userId = req.params.id;
+    const { username, email } = req.body;
+    const sql = `
+    UPDATE user 
+    SET username = ?, email = ?
+    WHERE id = ?;`;
+
+    db.query(sql, [username, email, userId], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send({ message: `User ${userId} has been updated.`, result });
+    });
+  },
+
   getAll(req, res) {
     const sql = `SELECT * FROM user;`;
 
@@ -8,6 +36,58 @@ const UserController = {
       if (err) throw err;
       console.log(result);
       res.send(result);
+    });
+  },
+
+  getById(req, res) {
+    const userId = req.params.id;
+    const sql = `SELECT * FROM user WHERE id = ?`;
+
+    db.query(sql, [userId], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  },
+
+  getAllWithOrders(req, res) {
+    const sql = `
+    SELECT 
+      u.username,
+      o.created_at AS order_date,
+      o.total AS total_price
+    FROM user AS u
+    INNER JOIN orders AS o
+    ON u.id = o.user_id`;
+
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(err);
+      res.send(result);
+    });
+  },
+
+  delete(req, res) {
+    const userId = req.params.id;
+
+    const sqlDeleteOrders = `DELETE FROM orders WHERE user_id = ?;`;
+    const sqlDeleteUser = `DELETE FROM user WHERE id = ?;`;
+
+    // We delete any order (due to RESTRICT in table creation)
+    db.query(sqlDeleteOrders, [userId], (err1, result1) => {
+      if (err1) throw err1;
+      console.log(result1);
+
+      // We delete the user
+      db.query(sqlDeleteUser, [userId], (err2, result2) => {
+        if (err2) throw err2;
+        console.log(result2);
+        res.send({
+          message: `User ${userId} and their orders have been deleted.`,
+          ordersDeleted: result1.affectedRows,
+          userDeleted: result2.affectedRows,
+        });
+      });
     });
   },
 };
--- routes/users.js
+++ routes/users.js
@@ -7,88 +7,18 @@ const UserController = require("../controllers/UserController");
 router.get("/", UserController.getAll);
 
 // GET USER BY ID
-router.get("/id/:id", (req, res) => {
-  const userId = req.params.id;
-  const sql = `SELECT * FROM user WHERE id = ?`;
-
-  db.query(sql, [userId], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send(result);
-  });
-});
+router.get("/id/:id", UserController.getById);
 
 // GET ALL USERS with ORDERS
-router.get("/with-orders", (req, res) => {
-  const sql = `
-    SELECT 
-      u.username,
-      o.created_at AS order_date,
-      o.total AS total_price
-    FROM user AS u
-    INNER JOIN orders AS o
-    ON u.id = o.user_id`;
-
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(err);
-    res.send(result);
-  });
-});
+router.get("/with-orders", UserController.getAllWithOrders);
 
 // ADD USER (by POST method)
-router.post("/create-user", (req, res) => {
-  const { username, email } = req.body;
-  const sql = `
-    INSERT INTO user (username, email)
-    VALUES (?, ?);`;
-
-  db.query(sql, [username, email], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send({ message: "New user registered successfully", result });
-  });
-});
+router.post("/create-user", UserController.create);
 
 // UPDATE USER (by PUT method)
-router.put("/id/:id", (req, res) => {
-  const userId = req.params.id;
-  const { username, email } = req.body;
-  const sql = `
-    UPDATE user 
-    SET username = ?, email = ?
-    WHERE id = ?;`;
-
-  db.query(sql, [username, email, userId], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send({ message: `User ${userId} has been updated.`, result });
-  });
-});
+router.put("/id/:id", UserController.update);
 
 // DELETE USER BY ID
-router.delete("/:id", (req, res) => {
-  const userId = req.params.id;
-
-  const sqlDeleteOrders = `DELETE FROM orders WHERE user_id = ?;`;
-  const sqlDeleteUser = `DELETE FROM user WHERE id = ?;`;
-
-  // We delete any order (due to RESTRICT in table creation)
-  db.query(sqlDeleteOrders, [userId], (err1, result1) => {
-    if (err1) throw err1;
-    console.log(result1);
-
-    // We delete the user
-    db.query(sqlDeleteUser, [userId], (err2, result2) => {
-      if (err2) throw err2;
-      console.log(result2);
-      res.send({
-        message: `User ${userId} and their orders have been deleted.`,
-        ordersDeleted: result1.affectedRows,
-        userDeleted: result2.affectedRows,
-      });
-    });
-  });
-});
+router.delete("/:id", UserController.delete);
 
 module.exports = router;
